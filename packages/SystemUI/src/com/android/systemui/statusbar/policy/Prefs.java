7221d4b71523882e843e5f08b72b61ed68235520
XXMrHyde <xxmrhyde@AwesomeMashine.(none)>
2013-05-03 14:50:15 +0200

    Fix derp

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java
@@ -38,6 +38,10 @@ public class Prefs {
         return context.getSharedPreferences(Prefs.SHARED_PREFS_NAME, Context.MODE_PRIVATE).edit();
     }
 
+    public static void setLastBatteryLevel(Context context, int level) {
+        edit(context).putInt(LAST_BATTERY_LEVEL, level).commit();
+    }
+
     public static int getLastBatteryLevel(Context context) {
         return read(context).getInt(LAST_BATTERY_LEVEL, 50);
     }
