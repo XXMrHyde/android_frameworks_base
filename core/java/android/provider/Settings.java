950aa82a2cca5ad140d9ae305966f1456b565f76
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-11-13 01:05:31 +1100

    [2/2] Framework/base: Lockscreen maximized widgets
    Framework part of maximizing lockscreen widgets
    
    Taken from cm and modified for kitkat
    
    Change-Id: I6a354d218fa5cc4603b92ba479d6aeba7f8f9c60

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2416,6 +2416,12 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * Weather to minimize lockscreen challenge on screen turned on
+         * @hide
+         */
+        public static final String LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
