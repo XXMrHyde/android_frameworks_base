1aab006f4183fe0112d14b51be5a6f1583e2706b
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-04-21 22:07:32 +0200

    Non intrusive incall ui , (1/3):
    Thanks to OmniRom

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3781,6 +3781,13 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
+        /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
         // DarkKat additions end
 
         // Settings moved to Settings.Secure
