235401d7738713d6c50591be355f942a0ad75f56
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-01-15 03:28:58 +0100

    Battery styles, (1/2):
    Using CyanogenMods implementation as a base:
    https://github.com/CyanogenMod/android_frameworks_base/commit/4cdfad4100af9be5b0562dbe67e9e3ffcc57fbc4
    https://github.com/CyanogenMod/android_frameworks_base/commit/a1bab878eb11ccb70df09c7876c7baa06fd0e49d
    https://github.com/CyanogenMod/android_frameworks_base/commit/040721140b6dfe6a4ae5c0db7251073163f19b5d
    https://github.com/CyanogenMod/android_frameworks_base/commit/0e31558822b1920ad39e4ba87f813b0bf823a61e
    https://github.com/CyanogenMod/android_frameworks_base/commit/9203171b83f530b2948557d0207c70799308c879
    
    - Battery styles:
      - Icon portrait
      - Icon landscape
      - Circle
      - Text only
      - Hidden
    - Text appearance:
      - Inside the icon/circle
      - Next to the icon/circle
      - Hidden
    - Charging animation (icon and circle):
      - Disabled
      - Slow
      - Normal
      - Fast
    - Option to show the circle dotted
    - Circle dot length
    - Circle dot interval
    - Fill color
    - Text color
    
    Charging bolt:
    - Only show the charging bolt,
      if text style is next to the icon/circle,
      text is hidden
      or charging animation is disabled
    
    Charging animation:
    - Use a color fill animation, (circle switched from offset animation)
    
    Additional text appearance:
    - Always show the text on the expanded status bar header,
      (even if hidden)
    - Don`t show the text on the status bar while charging, if text is hidden
    
    Icon/circle frame color:
    - Always use the fill color with a transparency of 60%, (Android default)
    
    Low battery status:
    - Alaways use 0xfff4511e (deep orange 600, default battery saver color) for
      icon/circle and text color if the battery status is below 16%

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -120,6 +120,7 @@ import com.android.internal.statusbar.StatusBarIcon;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.FontSizeUtils;
@@ -343,6 +344,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     // battery
     private BatteryMeterView mBatteryView;
+    private BatteryLevelTextView mBatteryLevel;
 
     // position
     int[] mPositionTmp = new int[2];
@@ -483,16 +485,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_CLOCK_DATE_COLOR),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_TEXT),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_CHARGING_ANIMATION),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_COLOR),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR),
+                    Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_SHOW_TICKER),
@@ -532,16 +525,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_CLOCK_DATE_COLOR))) {
                 updateClock();
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_TEXT))
-                || uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_CHARGING_ANIMATION))
-                || uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_COLOR))
-                || uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR))) {
-                if (mBatteryView != null) {
-                    mBatteryView.updateSettings();
-                }
+                    Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR))) {
+                updateBatteryLevelTextColor();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_SHOW_TICKER))) {
                 mTickerEnabled = Settings.System.getIntForUser(
@@ -872,6 +857,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNotificationIcons.setOverflowIndicator(mMoreIcon);
         mStatusBarContents = (LinearLayout)mStatusBarView.findViewById(R.id.status_bar_contents);
         mBatteryView = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
+        mBatteryLevel = (BatteryLevelTextView) mStatusBarView.findViewById(R.id.battery_level_text);
         mCenterClockLayout = (LinearLayout)mStatusBarView.findViewById(R.id.center_clock_layout);
 
         mStackScroller = (NotificationStackScrollLayout) mStatusBarWindow.findViewById(
@@ -1058,6 +1044,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mHeader.setBatteryController(mBatteryController);
         mBatteryView.setBatteryController(mBatteryController);
+        mBatteryLevel.setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setNextAlarmController(mNextAlarmController);
         mHeader.setWeatherController(mWeatherController);
@@ -1082,9 +1069,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         startGlyphRasterizeHack();
         updateClock();
-        if (mBatteryView != null) {
-            mBatteryView.updateSettings();
-        }
+        updateBatteryLevelTextColor();
         return mStatusBarView;
     }
 
@@ -2166,6 +2151,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             mClockView.updateSettings();
             mClockView.setVisibility(visibility);
+        }
+    }
+
+    private void updateBatteryLevelTextColor() {
+        if (mBatteryLevel != null) {
+            mBatteryLevel.setTextColor(false);
         }
     }
 
