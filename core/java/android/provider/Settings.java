cde80c5e65142457409f497d9300ce2b58acbdd5
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-02-08 16:19:31 +0100

    Configurable 0, 90, 180 and 270 degree rotation, (1/2):
    Thanks to CyanogenMod:
    https://github.com/CyanogenMod/android_frameworks_base/commit/45255743b8c30fc1437ec8d388b817839992976e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3076,6 +3076,19 @@ public final class Settings {
         public static final String POWER_MENU_SILENT = "power_menu_silent";
 
         /**
+         * Control the type of rotation which can be performed using the accelerometer
+         * if ACCELEROMETER_ROTATION is enabled.
+         * Value is a bitwise combination of
+         * 1 = 0 degrees (portrait)
+         * 2 = 90 degrees (left)
+         * 4 = 180 degrees (inverted portrait)
+         * 8 = 270 degrees (right)
+         * Setting to 0 is effectively orientation lock
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /**
          * Volume rocker wake
          * @hide
          */
