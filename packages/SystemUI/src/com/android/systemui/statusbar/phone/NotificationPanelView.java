425d0bac9708414f8444cb35d271273cdf127ef4
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-03-04 02:42:46 +0100

    Add visualizer to lock screen:
    Thanks to CyanogenMod:
    
    SystemUI: add visualizer to lock screen:
    https://github.com/CyanogenMod/android_frameworks_base/commit/86c508be6b1c432977615bd4e7223ac358eb0d6e
    
    SystemUI: fix crashes with custom lockscreen shortcuts:
    https://github.com/CyanogenMod/android_frameworks_base/commit/cd69d1bf59974b90b94fcb217105dbbe8a59cda1
    
    Changes:
    - Use the lock screen icon color for the visualizer color

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1065,6 +1065,7 @@ public class NotificationPanelView extends PanelView implements
     private void setKeyguardBottomAreaVisibility(int statusBarState,
             boolean goingToFullShade) {
         if (goingToFullShade) {
+            mKeyguardBottomArea.requestVisualizer(false, 0);
             mKeyguardBottomArea.animate().cancel();
             mKeyguardBottomArea.animate()
                     .alpha(0f)
@@ -1103,6 +1104,7 @@ public class NotificationPanelView extends PanelView implements
                         .setDuration(mStatusBar.getKeyguardFadingAwayDuration()/2)
                         .start();
             }
+            mKeyguardBottomArea.requestVisualizer(false, 0);
         } else if (mStatusBarState == StatusBarState.SHADE_LOCKED
                 && statusBarState == StatusBarState.KEYGUARD) {
             mKeyguardStatusView.animate().cancel();
@@ -1115,6 +1117,7 @@ public class NotificationPanelView extends PanelView implements
                     .setDuration(320)
                     .setInterpolator(PhoneStatusBar.ALPHA_IN)
                     .withEndAction(mAnimateKeyguardStatusViewVisibleEndRunnable);
+            mKeyguardBottomArea.requestVisualizer(true, 320);
         } else if (statusBarState == StatusBarState.KEYGUARD) {
             mKeyguardStatusView.animate().cancel();
             mKeyguardStatusViewAnimating = false;
@@ -1828,6 +1831,14 @@ public class NotificationPanelView extends PanelView implements
     }
 
     @Override
+    public void onSwipingAnimationFinished(boolean snappingBack) {
+        if (snappingBack) {
+            mKeyguardBottomArea.requestVisualizer(true, 300);
+        }
+    }
+
+
+    @Override
     public KeyguardAffordanceView getLeftIcon() {
         return getLayoutDirection() == LAYOUT_DIRECTION_RTL
                 ? mKeyguardBottomArea.getCameraView()
@@ -1942,6 +1953,9 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void setEmptyDragAmount(float amount) {
+        if (amount == 0 && mStatusBarState == StatusBarState.KEYGUARD) {
+            mKeyguardBottomArea.requestVisualizer(true, 300);
+        }
         float factor = 0.8f;
         if (mNotificationStackScroller.getNotGoneChildCount() > 0) {
             factor = 0.4f;
