7e8d714ad9eba889d127fc5c99083b0834247cb9
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-10-25 17:28:20 +0100

    StatusBarIconController: Add accidentally removed applyNotificationIconsTint():

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -424,6 +424,7 @@ public class StatusBarIconController implements Tunable {
         mSignalCluster.setIconTint(mIconTint, mDarkIntensity);
         mMoreIcon.setImageTintList(ColorStateList.valueOf(mIconTint));
         mBatteryMeterView.setDarkIntensity(mDarkIntensity);
+        applyNotificationIconsTint();
     }
 
     private void applyNotificationIconsTint() {
